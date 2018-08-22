449bd37172c6ca5898105be875843a54a8ab2f7e
Dolan Hêriş <dolanskurd@yandex.com>
2018-08-22 18:54:34 +0300

    Uploading the first version of Kurdish

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open('README.md', 'r', encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+	name='Kurdish',
+	version='1.0.0',
+	author='Dolan Hêriş',
+	author_email='dolanskurd@mail.com',
+	url='https://github.com/dolanskurd',
+	description=('Kurdish Language Library for converting characters and numbers in Persian, English and also Arabic to Kurdish and vice versa.'),
+	long_description = long_description,
+	keywords='Kurdish Language Module Library',
+	py_modules=['Kurdish'],
+	packages=setuptools.find_packages(),
+	classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Documentation',
+        'Topic :: Printing',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Editors :: Text Processing',
+        'Topic :: Text Processing :: Fonts',
+        'Topic :: Text Editors :: Word Processors',
+        'Topic :: Utilities',
+        'Intended Audience :: Developers',
+	],
+)
+
