40b74086ed11b288a28291fa5a8165c1b55a3ff7
Dolan Hêriş <dolanskurd@mail.com>
2019-05-24 21:13:23 +0300

    update to 1.1.4

--- setup.py
+++ setup.py
@@ -1,40 +1,42 @@
-
 from setuptools import setup
 from os import path
-from io import open
 
 
 myPath = path.abspath(path.dirname(__file__))
-with open(path.join(myPath, 'README.md'), encoding='utf-8') as f:
-	README = f.read()
+with open(path.join(myPath, "README.md"), encoding="utf-8") as f:
+    README = f.read()
 
 setup(
-    name='Kurdish',
-    version='1.1.0',
-    author='Dolan Hêriş',
-    author_email='dolanskurd@mail.com',
-    url='https://github.com/dolanskurd',
-    description=('Kurdish Language Library for converting characters and digits in Persian, English and Arabic to Kurdish and vice versa.'),
+    name="Kurdish",
+    version="1.1.4",
+    author="Dolan Hêriş",
+    author_email="dolanskurd@mail.com",
+    url="https://github.com/dolanskurd/Kurdish",
+    description=(
+        "Kurdish Language Library for transliteration from Arabic-based Kurdish to Latin-based Kurdish. It can also be used for converting characters and digits in Persian, English and Arabic to Kurdish and vice versa."
+    ),
     long_description=README,
-    long_description_content_type='text/markdown',
-    license='MIT',
-    keywords='Kurdish Language Module Library Converter Digits Characters',
-    py_modules=['ku'],
+    long_description_content_type="text/markdown",
+    license="MIT",
+    keywords="Kurdish Language Module Library Converter Digits Characters Persian Farsi Arabic Latin English Transliteration",
+    py_modules=["ku", "bizrokeRules", "punctuation", "vowelIssues"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Topic :: Documentation',
-        'Topic :: Printing',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Text Editors :: Text Processing',
-        'Topic :: Text Processing :: Fonts',
-        'Topic :: Text Editors :: Word Processors',
-        'Topic :: Utilities',
-        'Intended Audience :: Developers',
-]
+        "Development Status :: 4 - Beta",
+        "Topic :: Documentation",
+        "Topic :: Printing",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Editors :: Text Processing",
+        "Topic :: Text Processing :: Fonts",
+        "Topic :: Text Editors :: Word Processors",
+        "Topic :: Utilities",
+        "Intended Audience :: Developers",
+    ],
 )
-
 
